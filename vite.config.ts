ba8c6679c5993fa94f66a6d04ca69e1da821a9d3
Mohamed Jaasir B <mohamedjaasirb@gmail.com>
2026-06-17 19:08:26 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { TanStackStartVite } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
   plugins: [
-    react()
-  ]
+    TanStackStartVite(),
+    react(),
+  ],
 });
